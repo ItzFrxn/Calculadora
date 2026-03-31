3edabb3ac26e5c8b4c0214c5d8fbb49df157deea
Francisco Melchor <fmelchor_@hotmail.com>
2026-03-31 15:57:45 -0500

    Actualizacion de interfaz

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -12,11 +12,89 @@ namespace Calculadora
         static void Main(string[] args)
         {
             Console.Title = "Calculadora";
+            // Menu principal
             Console.WriteLine("=========================");
             Console.WriteLine("   Calculadora Basica");
             Console.WriteLine("=========================");
-            Console.WriteLine(" 1) Suma\n 2) Resta\n 3) Multiplicacion\n 4) Division\n 5) Opciones avanzadas\n x) Salir");
+            Console.WriteLine(" 1) Suma\n 2) Resta\n 3) Multiplicacion\n 4) Division\n 0) Salir");
             Console.WriteLine("=========================");
+            Console.Write("Opcion: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            // Operaciones
+            Console.WriteLine("=========================");
+            Console.WriteLine("   Calculadora Basica");
+            Console.WriteLine("=========================");
+            switch (opcion)
+            {
+                case 1:
+                    Console.Write(" Suma: ");
+                    double a = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Suma: {a} + ");
+                    double b = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Suma: {a} + {b} = {Suma(a, b)} ");
+                    break;
+                case 2:
+                    Console.Write(" Resta: ");
+                    double c = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Resta: {c} - ");
+                    double d = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Resta: {c} - {d} = {Resta(c, d)} ");
+                    break;
+                case 3:
+                    Console.Write(" Multiplicacion: ");
+                    double e = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Multiplicacion: {e} * ");
+                    double f = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Multiplicacion: {e} * {f} = {Multiplicacion(e, f)} ");
+                    break;
+                case 4:
+                    Console.Write(" Division: ");
+                    double g = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Division: {g} / ");
+                    double h = Convert.ToDouble(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("=========================");
+                    Console.WriteLine("   Calculadora Basica");
+                    Console.WriteLine("=========================");
+                    Console.Write($" Division: {g} / {h} = {Division(g, h)} ");
+                    break;
+                case 0:
+                    Console.WriteLine("Saliendo...");
+                    return;
+                default:
+                    Console.WriteLine("Opcion no valida.");
+                    break;
+            }
         }
     }
 }
