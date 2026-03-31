8151ddbf97c871ce4c59f5a4cbbfecf0c9baf5d1
Francisco Melchor <fmelchor_@hotmail.com>
2026-03-31 12:22:15 -0500

    Se agregaron operaciones basicas

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Calculadora.operaciones;
 
 namespace Calculadora
 {
@@ -10,11 +11,10 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite el primer número:");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Digite el segundo número:");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine($"El resultado de la suma es: {num1 + num2}");
+            Console.WriteLine(Suma(5, 3));
+            Console.WriteLine(Resta(5, 3));
+            Console.WriteLine(Multiplicacion(5, 3));
+            Console.WriteLine(Division(5, 3));
         }
     }
 }
---
+++ Calculadora/operaciones.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+	public class operaciones
+	{
+		public static double Suma(double a, double b)
+		{
+			return a + b;
+		}
+		public static double Resta(double a, double b)
+		{
+			return a - b;
+		}
+		public static double Multiplicacion(double a, double b)
+		{
+			return a * b;
+		}
+		public static double Division(double a, double b)
+		{
+			if (b == 0)
+			{
+				throw new DivideByZeroException("No se puede dividir por cero.");
+			}
+			return a / b;
+		}
+	}
+}
