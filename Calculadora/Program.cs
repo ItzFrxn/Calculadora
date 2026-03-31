52adfb33566940a81736e8735690c56ad66ffa86
Francisco Melchor <fmelchor_@hotmail.com>
2026-03-31 11:24:18 -0500

    Suma basica

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,6 +10,11 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite el primer número:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite el segundo número:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"El resultado de la suma es: {num1 + num2}");
         }
     }
 }
