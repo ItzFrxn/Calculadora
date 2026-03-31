1f2cf5d7e7df6db9e782ca567a867417764be33b
Francisco Melchor <fmelchor_@hotmail.com>
2026-03-31 17:02:42 -0500

    Actualización interfaz

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -14,59 +14,55 @@ namespace Calculadora
         {
             Console.Title = "Calculadora";
             // Menu principal
-            Menu();
-            Console.WriteLine(" 1) Suma\n 2) Resta\n 3) Multiplicacion\n 4) Division\n 0) Salir");
-            Console.WriteLine("=========================");
-            Console.Write("Opcion: ");
-            int opcion = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
+            Inicio(" 1) Suma\n 2) Resta\n 3) Multiplicacion\n 4) Division\n 0) Salir");
+            int opcion = Opcion();
 
             // Operaciones
-            Menu();
+            Titulo();
             switch (opcion)
             {
                 case 1:
                     Console.Write(" Suma: ");
                     double a = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Suma: {a} + ");
                     double b = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Suma: {a} + {b} = {Suma(a, b)} ");
                     break;
                 case 2:
                     Console.Write(" Resta: ");
                     double c = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Resta: {c} - ");
                     double d = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Resta: {c} - {d} = {Resta(c, d)} ");
                     break;
                 case 3:
                     Console.Write(" Multiplicacion: ");
                     double e = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Multiplicacion: {e} * ");
                     double f = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Multiplicacion: {e} * {f} = {Multiplicacion(e, f)} ");
                     break;
                 case 4:
                     Console.Write(" Division: ");
                     double g = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Division: {g} / ");
                     double h = Convert.ToDouble(Console.ReadLine());
                     Console.Clear();
-                    Menu();
+                    Titulo();
                     Console.Write($" Division: {g} / {h} = {Division(g, h)} ");
                     break;
                 case 0:
--- Calculadora/menu.cs
+++ Calculadora/menu.cs
@@ -8,11 +8,24 @@ namespace Calculadora
 {
     internal class menu
     {
-        public static void Menu()
+        public static void Titulo()
         {
             Console.WriteLine("=========================");
             Console.WriteLine("   Calculadora Basica");
             Console.WriteLine("=========================");
+        }
+        public static void Inicio(string text)
+        {
+            Titulo();
+            Console.WriteLine(text);
+            Console.WriteLine("=========================");
+        }
+        public static int Opcion()
+        {
+            Console.Write("Opcion: ");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            return opcion;
         }
     }
 }
